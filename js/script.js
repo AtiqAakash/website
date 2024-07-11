d1135f293b62bb7e743b66de2432baf07c9702f0
Md Atiqur Rahman Aakash <114288192+AtiqAakash@users.noreply.github.com>
2024-07-11 18:58:26 +0200

    Files uploaded

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".input", {
+  strings:["Atiqur Rahman Aakash.","An Embedded Software Developer."," A Drone Engineer."," A Photographer."],
+  typeSpeed: 40,
+  backSpeed: 15,
+  loop: true
+});
