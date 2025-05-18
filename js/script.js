3112591ff45199562c95cb5764d5cd4b436f5cc0
Md Atiqur Rahman Aakash <114288192+AtiqAakash@users.noreply.github.com>
2025-05-18 11:21:58 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".input", {
-  strings:["Atiqur Rahman Aakash.","An Embedded Software Developer."," A Drone Engineer."," A Photographer."],
-  typeSpeed: 40,
-  backSpeed: 15,
-  loop: true
-});
